7b052e6d2ec1ccc82bab930cd9fffbc862feab35
Antoine Eudes <antoine.eudes@gmail.com>
2020-03-17 19:29:38 +0000

    :sparkles: add logout button

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, StyleSheet, Image, TouchableHighlight } from 'react-native';
 
-const Home: React.FunctionComponent = () => (
-    <Text>Hello World</Text>
+interface Props {
+    logout: () => void;
+}
+
+const Home: React.FunctionComponent<Props> = ({ logout }: Props) => (
+    <View style={styles.header}>
+        <Text style={styles.title}>Hello World</Text>
+        <TouchableHighlight onPress={() => logout()}>
+            <Image style={styles.logoutIcon} source={require('../../icons/logout.png')} />
+        </TouchableHighlight>
+    </View>
 );
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 25
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    logoutIcon: {
+        width: 30,
+        height: 30,
+    }
+})
 
 export default Home;
